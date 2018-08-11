75e1411aee27ba762c2f96e1abfdde0f1d1b7eb2
TheKipmaster <fkipman@hotmail.com>
2018-08-11 12:53:55 -0300

    Implementa estrutura inicial do app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Header } from './components/common';
+
+const App = () => (
+  <View>
+    <Header title="To-Do List" />
+  </View>
+);
+
+export default App;
