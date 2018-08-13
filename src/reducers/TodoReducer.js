035ff54e3b07ecfc94ad9b9ad7403740a1716b73
TheKipmaster <fkipman@hotmail.com>
2018-08-13 20:37:02 -0300

    Acrescenta um monte de tarefas ao estado inicial

--- src/reducers/TodoReducer.js
+++ src/reducers/TodoReducer.js
@@ -4,7 +4,20 @@ import {
   DELETE
 } from '../actions/types';
 
-const INITIAL_STATE = { 'Wash Dishes': false };
+const INITIAL_STATE = {
+  'Wash Dishes': false,
+  'Mop Floors': false,
+  'Walk Dog': true,
+  'Clean Fridge': true,
+  'Shop For Groceries': false,
+  'Make Bed': true,
+  'Take a Shower': true,
+  'Take Out Trash': true,
+  'Mow Lawn': false,
+  'Cook Lunch': false,
+  'Finish Homework': false,
+  'Call Mom': false,
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
